8c0542f12e20e53f982cfeefca33cc9d52ef8eae
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-06-07 10:08:21 +0300

    added a manage file

---
+++ manage.py
@@ -0,0 +1,14 @@
+from app import create_app
+from flask_script import Manager,Server
+
+
+
+
+# Creating app instance
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+if __name__ == '__main__':
+    manager.run()
